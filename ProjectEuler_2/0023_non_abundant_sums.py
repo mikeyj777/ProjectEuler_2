1fbf1b955f42422f46328273ac4235bae81ec6ab
Michael James <u773253@emn.com>
2019-08-26 17:17:04 -0400

    update to proj 23

--- ProjectEuler_2/0023_non_abundant_sums.py
+++ ProjectEuler_2/0023_non_abundant_sums.py
@@ -2,7 +2,10 @@
 #
 # A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 #
-# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. 
+# By mathematical analysis, it can be shown that all integers greater than 28123 
+# can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the 
+# greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 #
 # Find the sum of all the positive integers which cannot be written as the
 # sum of two abundant numbers.
@@ -17,7 +20,7 @@ from datetime import datetime
 def sumofnumsnotsumsofabundants(n):
 
     #     divisors = np.arange(1, n // 2 + 1)
-
+    notpairs = np.arange(1, n+1)
     abundants = []
     sumsofabundantpairs = []
     temp = []
@@ -26,7 +29,7 @@ def sumofnumsnotsumsofabundants(n):
 
     for i in range(1, n + 1):
         temp = np.array([m for m in np.arange(1, i // 2 + 1) if i %
-                         m == 0 and not m == i])
+                         m == 0 and m < i])
         y = temp.sum()
 
         if i % 1000 == 0:
@@ -39,6 +42,7 @@ def sumofnumsnotsumsofabundants(n):
             abundants.append(i)
             sumsofabundantpairs.extend(temp)
             sumsofabundantpairs = list(dict.fromkeys(sumsofabundantpairs))
+            notpairs = np.asarray([x for x in notpairs if x not in sumsofabundantpairs])
 
     allthenums = range(1, n + 1)
 
